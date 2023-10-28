a9958492f7767f9125991075f876cac32cceaf96
mikigoalie <mikigoalie@seznam.cz>
2023-10-28 03:29:36 +0200

    types for config

--- shared/config.lua
+++ shared/config.lua
@@ -1,27 +1,30 @@
 lib.locale()
 Config = {}
+
+---@param displayContent players | societies | both
+---@param leftside boolean
+---@param primaryColor MantineColor
 Config.server = {
     displayContent = "both",
     leftside = true,
     primaryColor = GetConvar('ox:primaryColor', 'violet')
 }
 
+
+---@param society_label string
+---@param society_employee_count number
+---@param divider? string
 Config.societies = {
     {
-        society_label = "POLICE",
+        society_label = "LSPD",
         society_employee_count = 300,
-        divider = "Cigáni"
+    },
+    {
+        society_label = "LSSD",
+        society_employee_count = 300
     },
     {
         society_label = "EMS",
         society_employee_count = 300
     },
-    {
-        society_label = "POLICE",
-        society_employee_count = 300
-    },
-    {
-        society_label = "POLICE",
-        society_employee_count = 300
-    }
 }
